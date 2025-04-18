93887d128c67d58236d2cdcad695ead72e8af048
H3NRY B41T <151984495+H3NRY-B41T@users.noreply.github.com>
2025-04-19 03:10:11 +0500

    Update flag.js

--- pages/api/flag.js
+++ pages/api/flag.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   const cookies = req.headers.cookie || "";
 
   if (cookies.includes("role=admin")) {
-    res.status(200).json({ flag: "crumble{y0u_g0t_th3_c00k13}" });
+    res.status(200).json({ flag: "THM{CruMBLE_C00kieSsss}" });
   } else {
     res.status(403).json({ error: "Access Denied" });
   }
